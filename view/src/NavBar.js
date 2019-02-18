f209686aafe383634a7312ad466d037f7325fcbc
ChillMung9 <yuseon1216@gmail.com>
2019-02-18 17:48:52 -0500

    navbar update

--- view/src/NavBar.js
+++ view/src/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class NavBar extends Component {
     render() {
@@ -6,6 +7,11 @@ export default class NavBar extends Component {
             <nav className="navbar fixed-top navbar-light bg-light">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">{this.props.title}</a>
+                    <ul className="nav">
+                        <li className="nav-item">
+                            <Link to="/"><i className="fas fa-shopping-cart"></i></Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         )
